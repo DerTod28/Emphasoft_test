07ae2e0d6cf1b5847537671812fe93454df3f012
viktorsosnovskih <ads123321@mail.ru>
2023-02-28 21:18:20 +0800

    -- user signup/login added

--- src/booking/user/urls.py
+++ src/booking/user/urls.py
@@ -1,3 +1,11 @@
-urlpatterns = [
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from .views import CurrentUserView, RegisterView
+
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('signup/', RegisterView.as_view(), name='auth_signup'),
+    path('me/', CurrentUserView.as_view(), name='current_user')
 ]
--- src/booking/user/views.py
+++ src/booking/user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .serializers import UserSignupSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = UserSignupSerializer
+
+
+class CurrentUserView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSignupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
